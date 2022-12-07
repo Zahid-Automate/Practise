5e14eb31dfd1f7d18354f281457f9f3c7a0511db
mzahid <abdulzahid.01@gmail.com>
2022-12-06 23:40:09 -0800

    added a program with different logic

--- NMP2/src/main/java/report/PeakElement.java
+++ NMP2/src/main/java/report/PeakElement.java
@@ -18,6 +18,7 @@ public class PeakElement {
         PeakElement p1 = new PeakElement();
         p1.removeDuplicatesInArray();
         p1.removeRepeatedChar();
+        p1.removeRepeatedCharacters();
        
     }
     
@@ -52,9 +53,34 @@ public class PeakElement {
                 if (chars[i] != chars[i - 1]) {
                     sb.append(chars[i]);
                 }
-                System.out.println(sb);
             }
+            System.out.println(sb);
         }
     }
-   
+    
+    public void removeRepeatedCharacters() {
+        char str[] = "aaaaaaabbbcddddefggg".toCharArray();    
+        int n = str.length;
+        int i,j;
+        // Used as index in the modified string
+        int index = 0;
+        // Traverse through all characters
+        for (i = 0; i < n; i++)
+        {
+            // Check if str[i] is present before it
+            for (j = 0; j < i; j++)
+            {
+                if (str[i] == str[j])
+                {
+                    break;
+                }
+            }
+            // If not present, then add it to result.
+            if (j == i)
+            {
+                str[index++] = str[i];
+            }
+       }
+        System.out.println(String.valueOf(Arrays.copyOf(str, index)));
+    }
 }
